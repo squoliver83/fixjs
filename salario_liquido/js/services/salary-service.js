434cecdd61dc98bbfcf8fe27fb6f2898bfb601ba
Samuel Quaresma <squoliver@gmail.com>
2025-07-06 15:33:18 -0300

    Correção de texto

---
+++ salario_liquido/js/services/salary-service.js
@@ -0,0 +1,10 @@
+import * as taxService from "./tax-service.js";
+import * as pensionService from "./pension-service.js";
+
+export function netSalary(employee) {
+
+    const tax = taxService.tax(employee.grossSalary);
+    const pension = pensionService.discount(employee.grossSalary);
+
+    return employee.grossSalary - (tax + pension);
+}
